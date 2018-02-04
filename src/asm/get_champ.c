9f01ef64b6692465dbf0c5e1ddbf38532af9b74d
yoann guzman <yoann.guzman@gmail.com>
2018-02-04 18:31:56 +0100

    get_header

--- src/asm/get_champ.c
+++ src/asm/get_champ.c
@@ -33,18 +33,58 @@ int		get_nb_lines(char *file)
 	return (count);
 }
 
+void	epur_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+			str[i] = 0;
+		++i;
+	}
+}
+
 int		get_header(char **file, t_header *header)
 {
-//	champ->prog_name = ;
-//	champ->comment = ;
-//	champ->prog_size = 993;
+	char	**tab;
+
+	if ((tab = ft_strsplit(file[0], "\"")) == NULL)
+		return (ft_puterr("function ft_split fail in get header\n"));
+	epur_space(tab[0]);
+	if (tab == NULL || tab[0] == NULL || ft_strcmp(tab[0], ".name"))
+		return (ft_puterr(".name needed in header\n"));
+	if (tab[1] == NULL || tab[2] != NULL)
+		return (ft_puterr(".name needed just one name\n"));
+	if (ft_strlen(tab[1]) > PROG_NAME_LENGTH || ft_strlen(tab[1]) < 1)
+		return (ft_puterr("Prog name need char between 1 to PROG_NAME_LENGTH\n"));
+	ft_strcpy(header->prog_name, tab[1]);
+	free(tab[0]);
+	free(tab);
+	if ((tab = ft_strsplit(file[1], "\t\"")) == NULL)
+		return (ft_puterr("function ft_split fail in get header\n"));
+	epur_space(tab[0]);
+	if (tab == NULL || tab[0] == NULL || ft_strcmp(tab[0], ".comment"))
+		return (ft_puterr(".comment needed in header\n"));
+	if (tab[1] == NULL || tab[2] != NULL)
+	{
+		ft_putstr(tab[1]);
+		return (ft_puterr(".comment needed just one comment\n"));
+	}
+	if (ft_strlen(tab[1]) > COMMENT_LENGTH || ft_strlen(tab[1]) < 1)
+		return (ft_puterr("Comment need char between 1 to COMMENT_LENGTH\n"));
+	ft_strcpy(header->comment, tab[1]);
+	free(tab[0]);
+	free(tab);
 	return (0);
 }
 
 int		get_info_file(char **file, t_header *champ, t_list **inf_line)
 {
-	get_header(file, champ);
-	return (-1);
+	if (get_header(file, champ) == -1)
+		return (-1);
+	return (0);
 }
 
 int		get_champ(char *name, t_header *champ, t_list **inf_line)
@@ -65,5 +105,6 @@ int		get_champ(char *name, t_header *champ, t_list **inf_line)
 		return (-1);
 	if (get_info_file(file, champ, inf_line) == -1)
 		return (-1);
+	//champ->prog_size = ;
 	return (0);
 }
