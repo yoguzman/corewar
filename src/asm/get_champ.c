f7b49c8178a10b70f3bceaf009251d20f80adb16
yoann guzman <yoann.guzman@gmail.com>
2018-02-15 14:33:47 +0100

    sd

--- src/asm/get_champ.c
+++ src/asm/get_champ.c
@@ -54,11 +54,6 @@ int		get_header(char **file, t_header *header)
 	epur_space(tab[0]);
 	if (tab == NULL || tab[0] == NULL || ft_strcmp(tab[0], ".comment"))
 		return (ft_puterr(".comment needed in header\n"));
-	if (tab[1] == NULL || tab[2] != NULL)
-	{
-		ft_putstr(tab[1]);
-		return (ft_puterr(".comment needed just one comment\n"));
-	}
 	if (ft_strlen(tab[1]) > COMMENT_LENGTH || ft_strlen(tab[1]) < 1)
 		return (ft_puterr("Comment need char between 1 to COMMENT_LENGTH\n"));
 	ft_strcpy(header->comment, tab[1]);
@@ -74,6 +69,7 @@ int		get_info_file(char **file, t_header *champ, t_list **inf_line)
 	t_if	info_line;
 	t_if	*tmp;
 	int	i;
+	int		ret;
 
 	if (get_header(file, champ) == -1)
 		return (-1);
@@ -90,8 +86,10 @@ int		get_info_file(char **file, t_header *champ, t_list **inf_line)
 			++i;
 			continue ;
 		}
-		if (pars_instr(file[i], &info_line, i + 1) == -1)
+		if ((ret = pars_instr(file[i], &info_line, i + 1)) == -1)
 			return (-1);
+		else if (ret == 1)
+			continue ;
 		if (tmp == NULL)
 			bytes = 0;
 		else
