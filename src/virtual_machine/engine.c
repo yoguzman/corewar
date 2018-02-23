d0def9b8a02db6af80efab1560042c8bd5d66e68
Yoann GUZMAN <yguzman@e1r4p2.42.fr>
2018-02-23 16:27:46 +0100

    commentaire engine ajout de truc

--- src/virtual_machine/engine.c
+++ src/virtual_machine/engine.c
@@ -6,7 +6,7 @@
 /*   By: adauchy <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/23 14:22:57 by adauchy           #+#    #+#             */
-/*   Updated: 2018/02/23 15:06:31 by adauchy          ###   ########.fr       */
+/*   Updated: 2018/02/23 16:17:36 by yguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,23 @@ int			engine(t_corewar *vm)
 	while (TOUT LE MONDE NEST PAS DANS LA MORT)
 	{
 		
-		 * cycle_to_die--
-		 * SI cycle_to_die == 0
-		    verification de la vivance des champions
-			 Si NBR_LIVE est atteint
-               CYCLE_TO_DIE -= CYCLE_DELTA
-			cycle_to_dye = CYCLE_TO_DIE
-		 * cycle++
-		 * On va dans chaque proc
-		 * on decremente les cycles_to_exec
-		 * SI le cycle_to_exec arrive a son terme
-		    EXECUTION instruction
+		cycle_to_die--
+		SI cycle_to_die == 0
+			verification de la vivance des champions
+		Si NBR_LIVE est atteint
+			CYCLE_TO_DIE -= CYCLE_DELTA
+		cycle_to_dye = CYCLE_TO_DIE
+		cycle++
+		On va dans chaque proc
+			SI nous ne sommes pas sur une instructions connu
+				incrementer le pc de 1 octets
+			SI on arrive sur une instruction
+				set cycles_to_exec
+			SI on est sur une instruction
+				on decremente les cycles_to_exec
+			SI le cycle_to_exec arrive a son terme
+		    	EXECUTION instruction
+				incrementer le pc du nombre d octet de linstruction
 		 Si NCURSES ACTIVE
 		     NCURSES
 		 Si (DUMP ACTIVE ET cycles = dump_limit)
