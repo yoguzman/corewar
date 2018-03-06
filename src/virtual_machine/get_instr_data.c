6303f3f88a1b55675660c71840338550a1852159
Julien Coutarel <jcoutare@student.42.fr>
2018-03-06 12:33:23 +0100

    dfafadwf

--- src/virtual_machine/get_instr_data.c
+++ src/virtual_machine/get_instr_data.c
@@ -6,7 +6,7 @@
 /*   By: jcoutare <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 17:24:23 by jcoutare          #+#    #+#             */
-/*   Updated: 2018/03/02 19:25:12 by jcoutare         ###   ########.fr       */
+/*   Updated: 2018/03/06 12:29:00 by jcoutare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,46 @@
 #include "op.h"
 #include "libft.h"
 
-void	get_reg(t_corewar *vm, t_proc *lol, t_instr *instr,
-				int i)
+
+void	swap_32(unsigned long long *nb)
 {
-	instr->param[i] = vm->arena[lol->pc];
-	lol->pc += T_REG;
+	unsigned long long buf1;
+	unsigned long long buf2;
+
+	buf1 = ((*nb & 0xff000000) >> 24) | ((*nb & 0x00ff0000) >> 8);
+	buf2 = ((*nb & 0x0000ff00) << 8) | (*nb << 24);
+	*nb = buf1 | buf2;
 }
 
-void	get_ind(t_corewar *vm, t_proc *lol, t_instr *instr, int i)
+void	swap_16(unsigned long long *nb)
 {
-	//TOFILL
-	lol->pc += IND_SIZE;
+	*nb = ((*nb & 0xff) >> 8 | (*nb << 8));
 }
 
-void	get_dir(t_corewar *vm, t_proc *lol, t_instr *instr, int i)
+void	get_param(t_corewar *vm, t_proc *lol, t_instr *instr,
+			  int i)
 {
-// KOMEN ON FE
-	lol->pc += DIR_SIZE;
+	int	j;
+
+	j = 0;
+	instr->param[i] = 0;
+	while (j < instr->val_arg[i])
+	{
+		instr->param[i] += vm->arena[lol->pc];
+		instr->param[i] <<= 8;
+		++j;
+	}
+	if (instr->val_arg[i] == 2)
+		swap_16(&(instr->param[i]));
+	else if (instr->val_arg[i] == 4)
+		swap_32(&(instr->param[i]));
+	lol->pc += instr->val_arg[i];
+}
+
+void	get_one_arg(t_corewar *vm, t_proc *lol, t_instr *instr)
+{
+	instr->val_arg[0] = instr->op_tab[instr->opcode].parameter_types[instr->opcode];
+	get_param(vm, lol, instr, 0);
 }
 
 int		get_data(t_corewar *vm, t_proc *lol, t_instr *instr)
@@ -42,15 +65,10 @@ int		get_data(t_corewar *vm, t_proc *lol, t_instr *instr)
 		la_balade(vm , lol, instr);
 		return (0);
 	}
-	lol->pc++; //PCK ON A RECUP LOCTET DE CODAGE
+	lol->pc++;
 	while (i < instr->op_tab[instr->opcode].parameter_count)
 	{
-		if (instr->val_arg[i] == T_REG)
-			get_reg(vm, lol, instr, i);
-		else if (instr->val_arg[i] == T_DIR)
-			get_dir(vm, lol, instr, i);
-		else if (instr->val_arg[i] == T_IND)
-			get_ind(vm, lol, instr, i);
+		get_param(vm, lol, instr, i);
 		i++;
 	}
 	instr->tab_instr[instr->opcode](vm, lol, instr);
